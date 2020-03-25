6ee4cffbb90a9110de9acc7ec7b978ee13acfa8d
Chih-Ping Weng <chihping.weng@appier.com>
2020-03-25 10:21:02 +0800

    Add version text

--- app/src/main/java/com/appier/android/sample/primary/NavigationFragment.java
+++ app/src/main/java/com/appier/android/sample/primary/NavigationFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.appier.android.sample.R;
 
@@ -36,7 +37,13 @@ public class NavigationFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sample_navigation, container, false);
+        View layout = inflater.inflate(R.layout.fragment_sample_navigation, container, false);
+        TextView textVersion = layout.findViewById(R.id.text_version);
+        if (mPosition == 0) {
+            textVersion.setText("Appier SDK version : 1.0.0-rc5");
+        } else if (mPosition == 1) {
+            textVersion.setText("Mediation SDK version : 1.0.0-rc4");
+        }
+        return layout;
     }
 }
