1789a9393fbb5a8983752a953632badd3911cdc6
Yuan-Hung Huang <yuanhung.huang@appier.com>
2020-04-08 19:24:43 +0800

    add AppierBannerView integration sample

--- appier-sdk-sample/src/main/java/com/appier/android/sample/activity/sdk/BannerBasicActivity.java
+++ appier-sdk-sample/src/main/java/com/appier/android/sample/activity/sdk/BannerBasicActivity.java
@@ -12,6 +12,7 @@ import com.appier.android.sample.common.MyService;
 import com.appier.android.sample.common.MyServiceController;
 import com.appier.android.sample.common.SectionsPagerAdapter;
 import com.appier.android.sample.fragment.sdk.BannerBasicFragment;
+import com.appier.android.sample.fragment.sdk.BannerViewFragment;
 
 
 public class BannerBasicActivity extends BaseActivity {
@@ -29,24 +30,29 @@ public class BannerBasicActivity extends BaseActivity {
 
         mContext = this;
 
-        addTabbedViewPager(new SectionsPagerAdapter(getSupportFragmentManager(), new String[] {"Activity", "Application", "Service"}) {
+        addTabbedViewPager(new SectionsPagerAdapter(getSupportFragmentManager(), new String[] {"AppierBannerView", "Activity", "Application", "Service"}) {
             @Override
             @NonNull
             public Fragment getItem(int position) {
                 switch (position) {
                     case 0:
                         /*
+                         * Basic integration using AppierBannerView
+                         */
+                        return new BannerViewFragment();
+                    case 1:
+                        /*
                          * Basic integration from Activity Context
                          */
                         return BannerBasicFragment.newInstance(mContext);
 
-                    case 1:
+                    case 2:
                         /*
                          * Integration from Application Context
                          */
                         return BannerBasicFragment.newInstance(mContext.getApplicationContext());
 
-                    case 2:
+                    case 3:
                         /*
                          * Integration from Service Context
                          */
@@ -61,7 +67,7 @@ public class BannerBasicActivity extends BaseActivity {
                         throw new IndexOutOfBoundsException();
                 }
             }
-        });
+        }, true);
     }
 
     @Override
---
+++ appier-sdk-sample/src/main/java/com/appier/android/sample/fragment/sdk/BannerViewFragment.java
@@ -0,0 +1,80 @@
+package com.appier.android.sample.fragment.sdk;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.appier.ads.Appier;
+import com.appier.ads.AppierBannerAd;
+import com.appier.ads.AppierBannerView;
+import com.appier.ads.AppierError;
+import com.appier.android.sample.R;
+import com.appier.android.sample.fragment.BaseFragment;
+
+public class BannerViewFragment extends BaseFragment implements AppierBannerAd.EventListener {
+
+    private AppierBannerView mAppierBannerView1;
+    private AppierBannerView mAppierBannerView2;
+    private AppierBannerView mAppierBannerView3;
+
+    public BannerViewFragment() {}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_sdk_banner_view, container, false);
+    }
+
+    @Override
+    protected void onViewVisible(View view) {
+        mAppierBannerView1 = view.findViewById(R.id.appier_banner_view_320_50);
+        mAppierBannerView1.setAdDimension(320, 50);
+        mAppierBannerView1.setZoneId(getResources().getString(R.string.zone_320x50));
+        mAppierBannerView1.loadAd();
+
+        mAppierBannerView2 = view.findViewById(R.id.appier_banner_view_300_250);
+        mAppierBannerView2.setAdDimension(300, 250);
+        mAppierBannerView2.setZoneId(getResources().getString(R.string.zone_300x250));
+        mAppierBannerView2.loadAd();
+
+        mAppierBannerView3 = view.findViewById(R.id.appier_banner_view_320_480);
+        mAppierBannerView3.setAdDimension(320, 480);
+        mAppierBannerView3.setZoneId(getResources().getString(R.string.zone_320x480));
+        mAppierBannerView3.loadAd();
+
+    }
+
+    @Override
+    public void onDestroy() {
+        if (mAppierBannerView1 != null) {
+            mAppierBannerView1.destroy();
+        }
+        if (mAppierBannerView2 != null) {
+            mAppierBannerView2.destroy();
+        }
+        if (mAppierBannerView3 != null) {
+            mAppierBannerView3.destroy();
+        }
+        super.onDestroy();
+    }
+
+    @Override
+    public void onAdLoaded(AppierBannerAd appierBannerAd) {
+        Appier.log("[Sample App]", "[Banner]", "onAdLoaded()");
+    }
+
+    @Override
+    public void onAdNoBid(AppierBannerAd appierBannerAd) {
+        Appier.log("[Sample App]", "[Banner]", "onAdNoBid()");
+    }
+
+    @Override
+    public void onAdLoadFail(AppierError appierError, AppierBannerAd appierBannerAd) {
+        Appier.log("[Sample App]", "[Banner]", "onAdLoadFail()", appierError.toString());
+    }
+
+    @Override
+    public void onViewClick(AppierBannerAd appierBannerAd) {
+        Appier.log("[Sample App]", "[Banner]", "onViewClick()");
+    }
+}
