74694861b4b9e3bf34daa274d078133324f1f3d3
Yuan-Hung Huang <yuanhung.huang@appier.com>
2020-05-21 12:45:03 +0800

    refactor AppierNative samples and remove AppierNativeHelper

--- appier-sdk-sample/src/main/java/com/appier/android/sample/fragment/sdk/NativeListViewFragment.java
+++ appier-sdk-sample/src/main/java/com/appier/android/sample/fragment/sdk/NativeListViewFragment.java
@@ -8,11 +8,15 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.appier.ads.Appier;
 import com.appier.ads.AppierAdAdapter;
+import com.appier.ads.AppierError;
+import com.appier.ads.AppierNativeAd;
+import com.appier.ads.AppierNativeViewBinder;
 import com.appier.android.sample.R;
 import com.appier.android.sample.common.MyListViewAdapter;
 import com.appier.android.sample.fragment.BaseFragment;
-import com.appier.android.sample.helper.AppierNativeHelper;
+import com.appier.android.sample.helper.AppierAdHelper;
 
 import java.util.Arrays;
 
@@ -44,11 +48,97 @@ public class NativeListViewFragment extends BaseFragment {
         AppierAdAdapter mAppierAdAdapter = new AppierAdAdapter(arrayAdapter);
         mListView.setAdapter(mAppierAdAdapter);
 
-        AppierNativeHelper.insertAppierNativeToListView(
-                context, mDemoFlowController, mAppierAdAdapter, 2,
-                getResources().getString(R.string.zone_native),
-                R.layout.template_native_ad_compact_1
-        );
+        createNativeAdAndInsertInListView(mAppierAdAdapter, 2);
+    }
+
+    private void createNativeAdAndInsertInListView(AppierAdAdapter appierAdAdapter, int insertPosition){
+
+        /*
+         * Apply Appier global settings
+         */
+        AppierAdHelper.setAppierGlobal();
+
+        /*
+         * Create AppierNativeAd and display in the container using ViewBinder
+         */
+        AppierNativeViewBinder appierNativeViewBinder = new AppierNativeViewBinder.Builder(R.layout.template_native_ad_compact_1)
+                .mainImageId(R.id.native_main_image)
+                .iconImageId(R.id.native_icon_image)
+                .titleId(R.id.native_title)
+                .textId(R.id.native_text)
+                .callToActionId(R.id.native_cta)
+                .privacyInformationIconImageId(R.id.native_privacy_information_icon_image)
+                .build();
+
+        AppierNativeAd appierNativeAd = new AppierNativeAd(getActivity(), new EventListener(appierAdAdapter, insertPosition));
+        appierNativeAd.setViewBinder(appierNativeViewBinder);
+        appierNativeAd.setZoneId(getResources().getString(R.string.zone_native));
+        appierNativeAd.loadAd();
+
+    }
+
+    private class EventListener implements AppierNativeAd.EventListener {
+
+        private AppierAdAdapter appierAdAdapter;
+        private int insertPosition;
+
+        private EventListener(AppierAdAdapter adapter, int position) {
+            appierAdAdapter = adapter;
+            insertPosition = position;
+        }
+
+        @Override
+        public void onAdLoaded(AppierNativeAd appierNativeAd) {
+            Appier.log("[Sample App] onAdLoaded()");
+            View adView = appierNativeAd.getAdView();
+
+            try {
+                appierAdAdapter.insertAd(insertPosition, appierNativeAd);
+                mDemoFlowController.notifyAdBid();
+
+            } catch (Exception e) {
+                Appier.log("[Sample App] Fail to insert ad into list. Maybe the position is out of bound or is already used.");
+                mDemoFlowController.notifyAdError(AppierError.UNKNOWN_ERROR);
+            }
+        }
+
+        @Override
+        public void onAdNoBid(AppierNativeAd appierNativeAd) {
+            Appier.log("[Sample App] onAdNoBid()");
+            mDemoFlowController.notifyAdNoBid();
+        }
+
+        @Override
+        public void onAdLoadFail(AppierError appierError, AppierNativeAd appierNativeAd) {
+            Appier.log("[Sample App] onAdLoadFail()", appierError.toString());
+            mDemoFlowController.notifyAdError(appierError);
+        }
+
+        @Override
+        public void onAdShown(AppierNativeAd appierNativeAd) {
+            Appier.log("[Sample App] onAdShown()");
+        }
+
+        @Override
+        public void onImpressionRecorded(AppierNativeAd appierNativeAd) {
+            Appier.log("[Sample App] onImpressionRecorded()");
+        }
+
+        @Override
+        public void onImpressionRecordFail(AppierError appierError, AppierNativeAd appierNativeAd) {
+            Appier.log("[Sample App] onImpressionRecordFail()", appierError.toString());
+        }
+
+        @Override
+        public void onAdClick(AppierNativeAd appierNativeAd) {
+            Appier.log("[Sample App] onAdClick()");
+        }
+
+        @Override
+        public void onAdClickFail(AppierError appierError, AppierNativeAd appierNativeAd) {
+            Appier.log("[Sample App] onAdClickFail()");
+        }
+
     }
 
 }
