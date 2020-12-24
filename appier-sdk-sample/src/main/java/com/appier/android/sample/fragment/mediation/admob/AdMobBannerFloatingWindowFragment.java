272ea8dc75cb245dff50af66ba9e57d36c5e3eb5
Justin Kuo <justin.kuo@appier.com>
2020-12-24 17:33:50 +0800

    add AdMob banner examples

---
+++ appier-sdk-sample/src/main/java/com/appier/android/sample/fragment/mediation/admob/AdMobBannerFloatingWindowFragment.java
@@ -0,0 +1,113 @@
+package com.appier.android.sample.fragment.mediation.admob;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import com.appier.ads.Appier;
+import com.appier.ads.common.AppierDataKeys;
+import com.appier.ads.common.Dimension;
+import com.appier.android.sample.R;
+import com.appier.android.sample.fragment.BaseFloatingWindowFragment;
+import com.appier.android.sample.helper.AppierAdHelper;
+import com.appier.mediation.admob.ads.AppierBanner;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.LoadAdError;
+
+public class AdMobBannerFloatingWindowFragment extends BaseFloatingWindowFragment {
+
+    private LinearLayout mAdContainer;
+    private AdView mAdView;
+
+    public AdMobBannerFloatingWindowFragment() {}
+
+    protected void loadAdInContainer(LinearLayout adContainer) {
+
+        Context context = getActivity();
+        mAdContainer = adContainer;
+
+        /*
+         * Apply Appier global settings
+         */
+        AppierAdHelper.setAppierGlobal();
+
+        final String ADMOB_AD_UNIT_ID = getString(R.string.admob_adunit_banner_300x250);
+        final int AD_WIDTH = 300;
+        final int AD_HEIGHT = 250;
+
+        /*
+         * Initialize AdView and load banner
+         *
+         * To enable Appier AdMob Mediation, the AdUnit requires at least one "Custom event",
+         * with the following settings:
+         *
+         *   "Custom event class": "com.appier.mediation.admob.ads.AppierBanner".
+         *   "Custom event parameter":  { "zoneId": "<THE ZONE ID PROVIDED BY APPIER>" }
+         *
+         */
+
+        Bundle localExtras = new Bundle();
+        localExtras.putInt(AppierDataKeys.AD_WIDTH_LOCAL, AD_WIDTH);
+        localExtras.putInt(AppierDataKeys.AD_HEIGHT_LOCAL, AD_HEIGHT);
+        localExtras.putString(AppierDataKeys.AD_UNIT_ID_LOCAL, ADMOB_AD_UNIT_ID);
+
+        mAdView = new AdView(context);
+
+        // set layout parameter to remove white margin
+        mAdView.setLayoutParams(new LinearLayout.LayoutParams(
+                Dimension.dipsToIntPixels(AD_WIDTH, context),
+                Dimension.dipsToIntPixels(AD_HEIGHT, context)
+        ));
+
+        // Load Ad!
+        mAdView.setAdUnitId(ADMOB_AD_UNIT_ID);
+        mAdView.setAdSize(AdSize.MEDIUM_RECTANGLE);
+        mAdView.setAdListener(new AdMobBannerAdListener());
+        mAdView.loadAd(new AdRequest.Builder()
+                .addCustomEventExtrasBundle(AppierBanner.class, localExtras)
+                .build());
+
+    }
+
+    @Override
+    protected void destroyAdView() {
+        if (mAdView != null) {
+            mAdView.destroy();
+        }
+    }
+
+    /*
+     * Customize AdListener functions to handle event callbacks
+     */
+
+    public class AdMobBannerAdListener extends AdListener {
+        @Override
+        public void onAdLoaded() {
+            Appier.log("[Sample App]", "onAdLoaded()");
+            if (mAdContainer != null) {
+                mDemoFlowController.notifyAdBid();
+
+                // Display banner in parent container
+                mAdContainer.removeAllViews();
+                mAdContainer.addView(mAdView);
+            }
+        }
+
+        @Override
+        public void onAdFailedToLoad(LoadAdError loadAdError) {
+            Appier.log("[Sample App]", "onAdFailedToLoad():", loadAdError.getCode(), loadAdError.getMessage());
+            mDemoFlowController.notifyAdNoBid();
+            if (mFloatViewManager != null) {
+                mFloatViewManager.close();
+            }
+        }
+
+        @Override
+        public void onAdClicked() {
+            Appier.log("[Sample App]", "onAdClicked()");
+        }
+    }
+}
---
+++ appier-sdk-sample/src/main/java/com/appier/android/sample/fragment/navigation/AdMobMediationNavigationFragment.java
@@ -0,0 +1,108 @@
+package com.appier.android.sample.fragment.navigation;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Pair;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.appier.ads.AppierPredictor;
+import com.appier.android.sample.R;
+import com.appier.android.sample.activity.BaseActivity;
+import com.appier.android.sample.activity.mediation.admob.AdMobBannerBasicActivity;
+import com.appier.android.sample.activity.mediation.admob.AdMobBannerFloatingWindowActivity;
+import com.appier.android.sample.common.NavigationAdapter;
+import com.appier.android.sample.fragment.BaseFragment;
+import com.appier.android.sample.helper.AppierAdHelper;
+import com.appier.mediation.admob.AppierAdapterConfiguration;
+import com.google.android.gms.ads.MobileAds;
+import com.appier.mediation.admob.AppierAdUnitIdentifier;
+import com.appier.mediation.admob.AppierPredictHandler;
+
+public class AdMobMediationNavigationFragment extends BaseFragment {
+    public AdMobMediationNavigationFragment() {}
+
+    @Override
+    protected void onViewVisible(View view) {
+
+    }
+
+    public static AdMobMediationNavigationFragment newInstance(String title) {
+        AdMobMediationNavigationFragment fragment = new AdMobMediationNavigationFragment();
+        fragment.setTitleArgs(title);
+        return fragment;
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View layout = inflater.inflate(R.layout.fragment_primary_navigation, container, false);
+
+        // Appier Predict Button
+        Button mButtonPredict = layout.findViewById(R.id.button_predict);
+        mButtonPredict.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*
+                 * (Optional) Do Appier Predict
+                 *
+                 * It is recommended to call predictAd() at the previous activity before rendering Ads to achieve
+                 * best performance.
+                 */
+
+                // Remember to call helper functions to apply GDPR and COPPA consent before Appier predict
+                AppierAdHelper.setAppierGlobal();
+
+                // Create AppierPredictor instance and apply predict to all required AdUnits
+                AppierPredictor predictor = new AppierPredictor(getContext(), new AppierPredictHandler(getContext()));
+
+                predictor.predictAd(new AppierAdUnitIdentifier(getString(R.string.admob_adunit_banner_300x250)));
+            }
+        });
+
+        initializeAdMobMediationView(layout);
+        return layout;
+    }
+
+    private void initializeAdMobMediationView(View layout) {
+        initializeNavigationList(
+                layout.findViewById(R.id.secondary_nav_banner),
+                new Pair[] {
+                        new Pair<>("Banner - basic format", AdMobBannerBasicActivity.class),
+                        new Pair<>("Banner - in a floating window", AdMobBannerFloatingWindowActivity.class)
+                }
+        );
+        TextView textVersion = layout.findViewById(R.id.text_version);
+        textVersion.setText(
+                "Appier SDK version : " + AppierAdapterConfiguration.getNetworkSdkVersion() + "\n" +
+                "AdMob Mediation SDK version : " + AppierAdapterConfiguration.getMediationVersion() + "\n" +
+                "AdMob SDK version : " + MobileAds.getVersionString()
+        );
+    }
+
+    // TODO: extract me
+    private void initializeNavigationList(View view, Pair<String, Class<?>>[] navigations) {
+        ListView listView = (ListView) view;
+        final String title = this.getTitle();
+        final NavigationAdapter navigationAdapter = new NavigationAdapter(getContext());
+        for (Pair<String, Class<?>> navigation: navigations) {
+            navigationAdapter.add(navigation);
+        }
+        listView.setAdapter(navigationAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> paramAdapterView, View paramView, int position, long paramLong) {
+                Pair<String, Class<?>> item = navigationAdapter.getItem(position);
+                Intent intent = new Intent(getContext(), item.second);
+                intent.putExtra(BaseActivity.EXTRA_TITLE, item.first);
+                intent.putExtra(BaseActivity.EXTRA_SUB_TITLE, title);
+                startActivity(intent);
+            }
+        });
+    }
+}
